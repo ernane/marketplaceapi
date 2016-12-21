5f32cce2616f2766ade03b23e1bc29d0542cdb9a
Ernane Sena <ernane.sena@gmail.com>
2016-12-21 18:51:41 -0300

    Adds shoulda matchers for spec refactors

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,4 +12,6 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
   config.infer_spec_type_from_file_location!
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
 end
