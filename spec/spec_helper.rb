0139eaa49eb1b22483e6f8cf153ea85a569a8e1f
Ernane Sena <ernane.sena@gmail.com>
2016-12-30 21:29:40 -0300

    Refactors test headers for each request

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,4 +18,9 @@ RSpec.configure do |config|
   config.include(Shoulda::Matchers::ActionController, type: :controller)
   config.include(Devise::Test::ControllerHelpers, type: :controller)
   config.include(Request::JsonHelpers, type: :controller)
+  config.include(Request::HeadersHelpers, type: :controller)
+
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
 end
