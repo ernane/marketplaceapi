24c57965b40d1a768109f4435e4808a6971e7cb4
Ernane Sena <ernane.sena@gmail.com>
2016-12-30 21:18:13 -0300

    Refactors the json parse method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -11,10 +11,11 @@ RSpec.configure do |config|
   config.include Rails.application.routes.url_helpers
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
-  config.order = "random"
+  config.order = 'random'
   config.infer_spec_type_from_file_location!
   config.include(Shoulda::Matchers::ActiveModel, type: :model)
   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
   config.include(Shoulda::Matchers::ActionController, type: :controller)
   config.include(Devise::Test::ControllerHelpers, type: :controller)
+  config.include(Request::JsonHelpers, type: :controller)
 end
