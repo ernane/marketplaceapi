a76854341290b22d06cb70e1a82207e1a27b57e5
Ernane Sena <ernane.sena@gmail.com>
2017-01-21 12:03:25 -0300

    Adds public product endpoints(show, index)

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
 require 'api_constraints'
 
 Rails.application.routes.draw do
   mount SabisuRails::Engine => '/sabisu_rails'
   devise_for :users, skip: [:registrations]
-  #devise_for :users
+  # devise_for :users
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, only: [:show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
+      resources :products, only: [:show, :index]
     end
   end
 end
