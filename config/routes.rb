d917817e20091ee2ca520dbd6f878338ae4e0783
Ernane Sena <ernane.sena@gmail.com>
2017-01-29 15:32:52 -0300

    Adds the products create, update and destroy action nested on the user resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 require 'api_constraints'
 
 Rails.application.routes.draw do
@@ -7,7 +6,9 @@ Rails.application.routes.draw do
   # devise_for :users
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, only: [:show, :create, :update, :destroy]
+      resources :users, only: [:show, :create, :update, :destroy] do
+        resources :products, only: [:create, :update, :destroy]
+      end
       resources :sessions, only: [:create, :destroy]
       resources :products, only: [:show, :index]
     end
