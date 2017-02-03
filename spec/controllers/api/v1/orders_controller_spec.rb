15f6f298f7384cbed3e696abe52cef49a5091bf7
Ernane Sena <ernane.sena@gmail.com>
2017-02-03 20:39:35 -0300

    Adds the create method for the orders controller

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
 describe Api::V1::OrdersController do
+  before(:each) do
+    @current_user = FactoryGirl.create :user
+    api_authorization_header @current_user.auth_token
+  end
+
   describe 'GET #index' do
     before(:each) do
-      current_user = FactoryGirl.create :user
-      api_authorization_header current_user.auth_token
-      4.times { FactoryGirl.create :order, user: current_user }
-      get :index, user_id: current_user.id
+      4.times { FactoryGirl.create :order, user: @current_user }
+      get :index, user_id: @current_user.id
     end
 
     it 'returns 4 order records from the user' do
@@ -19,10 +22,8 @@ describe Api::V1::OrdersController do
 
   describe 'GET #show' do
     before(:each) do
-      current_user = FactoryGirl.create :user
-      api_authorization_header current_user.auth_token
-      @order = FactoryGirl.create :order, user: current_user
-      get :show, user_id: current_user.id, id: @order.id
+      @order = FactoryGirl.create :order, user: @current_user
+      get :show, user_id: @current_user.id, id: @order.id
     end
 
     it 'returns the user order record matching the id' do
@@ -31,5 +32,21 @@ describe Api::V1::OrdersController do
     end
 
     it { should respond_with 200 }
+  end
+
+  describe 'POST #create' do
+    before(:each) do
+      product_1 = FactoryGirl.create :product
+      product_2 = FactoryGirl.create :product
+      order_params = { product_ids: [product_1.id, product_2.id] }
+      post :create, user_id: @current_user.id, order: order_params
+    end
+
+    it 'returns the just user order record' do
+      order_response = json_response[:order]
+      expect(order_response[:id]).to be_truthy
+    end
+
+    it { should respond_with 201 }
   end
 end
